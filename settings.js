ec758c2da84dec1d2b480352bf93619e4a4a94ec
Beast-sta-bot12 <ibitoyepaul00@gmail.com>
2025-01-05 00:16:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "9034661538"
 
-global.bankowner = "FADARE"
+global.bankowner = "PETER"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
